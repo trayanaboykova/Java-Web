1332966294556ca4c582fa221c4eaa2bc5283458
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-02 15:36:03 +0200

    Create User.java

---
+++ smart-wallet-application/src/main/java/app/user/model/User.java
@@ -0,0 +1,54 @@
+package app.user.model;
+
+import app.subscription.model.Subscription;
+import app.wallet.model.Wallet;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String profilePicture;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Enumerated(EnumType.STRING)
+    private Country country;
+
+    private boolean isActive;
+
+    @Column(nullable = false)
+    private LocalDateTime createdOn;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedOn;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "owner")
+    private List<Subscription> subscriptions = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "owner")
+    private List<Wallet> wallets = new ArrayList<>();
+
+}
