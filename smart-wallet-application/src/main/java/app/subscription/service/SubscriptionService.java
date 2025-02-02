b4e7ef10ccf57db6a87f89bd6b86bcc869a0ed16
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-02 19:01:30 +0200

    Update SubscriptionService.java

--- smart-wallet-application/src/main/java/app/subscription/service/SubscriptionService.java
+++ smart-wallet-application/src/main/java/app/subscription/service/SubscriptionService.java
@@ -1,13 +1,49 @@
 package app.subscription.service;
 
+import app.subscription.model.Subscription;
+import app.subscription.model.SubscriptionPeriod;
+import app.subscription.model.SubscriptionStatus;
+import app.subscription.model.SubscriptionType;
+import app.subscription.repository.SubscriptionRepository;
 import app.user.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Slf4j
 @Service
 public class SubscriptionService {
 
-    public void createDefaultSubscription(User user) {
+    private final SubscriptionRepository subscriptionRepository;
 
+    public SubscriptionService(SubscriptionRepository subscriptionRepository) {
+        this.subscriptionRepository = subscriptionRepository;
+    }
+
+    public void createDefaultSubscription(User user) {
+        
+        Subscription subscription =  subscriptionRepository.save(initializeSubscription(user));
+
+        log.info("Successfully created a new subscription with id [%s] and type [%s].".formatted(subscription.getId(), subscription.getType()));
+
+    }
+
+    private Subscription initializeSubscription(User user) {
+
+        LocalDateTime now = LocalDateTime.now();
+
+        return Subscription.builder()
+                .owner(user)
+                .status(SubscriptionStatus.ACTIVE)
+                .period(SubscriptionPeriod.MONTHLY)
+                .type(SubscriptionType.DEFAULT)
+                .price(new BigDecimal("0.00"))
+                .renewalAllowed(true)
+                .createdOn(now)
+                .completedOn(now.plusMonths(1))
+                .build();
     }
 
 }
