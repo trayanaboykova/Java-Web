dd9e89d4e0f3d93b4bb493bca183e2fd87beff8e
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-02 18:53:01 +0200

    Update WalletService.java

--- smart-wallet-application/src/main/java/app/wallet/service/WalletService.java
+++ smart-wallet-application/src/main/java/app/wallet/service/WalletService.java
@@ -1,12 +1,47 @@
 package app.wallet.service;
 
 import app.user.model.User;
+import app.wallet.model.Wallet;
+import app.wallet.model.WalletStatus;
+import app.wallet.repository.WalletRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Currency;
+
+@Slf4j
 @Service
 public class WalletService {
 
+    private final WalletRepository walletRepository;
+
+    @Autowired
+    public WalletService(WalletRepository walletRepository) {
+        this.walletRepository = walletRepository;
+    }
+
     public void createNewWallet(User user) {
 
+        Wallet wallet = initializeWallet(user);
+
+        log.info("Successfully created new wallet with id [%s] and balance [%.2f].".formatted(wallet.getId(), wallet.getBalance()));
+
+        walletRepository.save(wallet);
+
+    }
+
+    private Wallet initializeWallet(User user) {
+
+        return Wallet.builder()
+                .owner(user)
+                .status(WalletStatus.ACTIVE)
+                .balance(new BigDecimal("20.00"))
+                .currency(Currency.getInstance("EUR"))
+                .createdOn(LocalDateTime.now())
+                .updatedOn(LocalDateTime.now())
+                .build();
     }
 }
