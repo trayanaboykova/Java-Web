c0a2b79e2cda3b48fe8691d9f58c0ceae94c5e43
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-02 15:36:07 +0200

    Create Wallet.java

---
+++ smart-wallet-application/src/main/java/app/wallet/model/Wallet.java
@@ -0,0 +1,35 @@
+package app.wallet.model;
+
+import app.user.model.User;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Currency;
+import java.util.UUID;
+
+@Entity
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    private User owner;
+
+    @Column(nullable = false)
+    private WalletStatus status;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    @Column(nullable = false)
+    private Currency currency;
+
+    @Column(nullable = false)
+    private LocalDateTime createdOn;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedOn;
+}
