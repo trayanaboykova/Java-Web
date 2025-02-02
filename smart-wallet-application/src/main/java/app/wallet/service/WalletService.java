505f5d90f53dd5a2dd9ff0acaa2f72df6fc3d63e
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-02 18:24:19 +0200

    Create WalletService.java

---
+++ smart-wallet-application/src/main/java/app/wallet/service/WalletService.java
@@ -0,0 +1,12 @@
+package app.wallet.service;
+
+import app.user.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletService {
+
+    public void createNewWallet(User user) {
+
+    }
+}
