31507431ea4fc6ee1e920bd071fdfa8def05fb37
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-02 18:24:17 +0200

    Create SubscriptionService.java

---
+++ smart-wallet-application/src/main/java/app/subscription/service/SubscriptionService.java
@@ -0,0 +1,13 @@
+package app.subscription.service;
+
+import app.user.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubscriptionService {
+
+    public void createDefaultSubscription(User user) {
+
+    }
+
+}
