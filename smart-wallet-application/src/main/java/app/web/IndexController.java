1408242b53503beecc200fb40e11600dfc58238d
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-04 11:43:55 +0200

    Create IndexController.java

---
+++ smart-wallet-application/src/main/java/app/web/IndexController.java
@@ -0,0 +1,56 @@
+package app.web;
+
+import app.user.model.User;
+import app.user.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
+
+@Controller
+@RequestMapping
+public class IndexController {
+
+    private final UserService userService;
+
+    public IndexController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //когато искаме да изпращаме модел атрибути
+//    @GetMapping("/")
+//    public ModelAndView getIndexPage(){
+//       return new ModelAndView("index");
+//    }
+
+    // когато искаме просто да върнем view
+    @GetMapping("/")
+    public String getIndexPage() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "register";
+    }
+
+    @GetMapping("/home")
+    public ModelAndView getHomePage() {
+
+        User user = userService.getById(UUID.fromString("3467407c-9663-4fc7-a771-9acb7a6f721f")); //TODO: remove hardcoding
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        modelAndView.addObject("user", user);
+
+        return modelAndView;
+    }
+
+}
