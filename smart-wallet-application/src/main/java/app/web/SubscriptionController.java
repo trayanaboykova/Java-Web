7b2ea80df15db0f751b5f63239fa5d8a60f4c892
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-04 14:30:01 +0200

    Create SubscriptionController.java

---
+++ smart-wallet-application/src/main/java/app/web/SubscriptionController.java
@@ -0,0 +1,41 @@
+package app.web;
+
+import app.subscription.repository.SubscriptionRepository;
+import app.user.model.User;
+import app.user.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/subscriptions")
+public class SubscriptionController {
+
+    private final UserService userService;
+
+    public SubscriptionController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String getUpgradePage() {
+       return "upgrade";
+   }
+
+    @GetMapping("/history")
+    public ModelAndView getUserSubscriptions() {
+
+        User user = userService.getById(UUID.fromString("3467407c-9663-4fc7-a771-9acb7a6f721f"));
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("subscription-history");
+        modelAndView.addObject("user", user);
+
+        return modelAndView;
+    }
+
+}
