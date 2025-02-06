713e5e0aaffc541258411adb68ec8fb0e7fd28f4
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-06 20:03:15 +0200

    Update SessionCheckInterceptor.java

--- smart-wallet-application/src/main/java/app/security/SessionCheckInterceptor.java
+++ smart-wallet-application/src/main/java/app/security/SessionCheckInterceptor.java
@@ -1,5 +1,7 @@
 package app.security;
 
+import app.user.model.User;
+import app.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -8,11 +10,18 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 @Component
 public class SessionCheckInterceptor  implements HandlerInterceptor {
 
     private final Set<String> UNAUTHENTICATED_ENDPOINTS = Set.of("/", "/login", "/register");
+
+    private final UserService userService;
+
+    public SessionCheckInterceptor(UserService userService) {
+        this.userService = userService;
+    }
 
     // Този метод ще се изпълни преди всяка заявка
     // HttpServletRequest request - заяката, която се праща към нашето приложение
@@ -32,6 +41,16 @@ public class SessionCheckInterceptor  implements HandlerInterceptor {
         HttpSession currentUserSession = request.getSession(false);
         if (currentUserSession == null) {
             response.sendRedirect("/login");
+            return false;
+        }
+
+        UUID userId = (UUID) currentUserSession.getAttribute("user_id");
+        User user = userService.getById(userId);
+
+        if (!user.isActive()) {
+
+            currentUserSession.invalidate();
+            response.sendRedirect("/");
             return false;
         }
 
