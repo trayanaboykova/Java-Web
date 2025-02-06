735248990f408d3e860acd5eb91026ffbcb5da3d
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-06 15:16:40 +0200

    Create WebMvcConfiguration.java

---
+++ smart-wallet-application/src/main/java/app/config/WebMvcConfiguration.java
@@ -0,0 +1,22 @@
+package app.config;
+
+import app.security.SessionCheckInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private SessionCheckInterceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/images/**");
+    }
+}
