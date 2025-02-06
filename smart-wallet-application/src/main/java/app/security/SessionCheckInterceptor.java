63d0c4db0cb55bec5d6f066d29fc5b0cb17c4dbb
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-06 15:11:19 +0200

    Create SessionCheckInterceptor.java

---
+++ smart-wallet-application/src/main/java/app/security/SessionCheckInterceptor.java
@@ -0,0 +1,40 @@
+package app.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class SessionCheckInterceptor  implements HandlerInterceptor {
+
+    private final Set<String> UNAUTHENTICATED_ENDPOINTS = Set.of("/", "/login", "/register");
+
+    // Този метод ще се изпълни преди всяка заявка
+    // HttpServletRequest request - заяката, която се праща към нашето приложение
+    // HttpServletResponse response - отговор, който връщаме
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        // Endpoint
+        String endpoint = request.getServletPath();
+        if (UNAUTHENTICATED_ENDPOINTS.contains(endpoint)) {
+            // Ако иска да достъпи ендпойнт, за който не ни трябва сесия, пускаме заявката напред да се обработи
+            return true;
+        }
+
+        // request.getSession(); - взема сесията, ако няма, се създава нова!
+        // request.getSession(false); - взема сесията, ако има, ако пък няма, се връща null!
+        HttpSession currentUserSession = request.getSession(false);
+        if (currentUserSession != null) {
+            response.sendRedirect("/login");
+            return false;
+        }
+
+        return true;
+    }
+}
