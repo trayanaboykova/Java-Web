07f7a8490627079e189c750c08b074de0812b76e
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-07 10:28:07 +0200

    subscription upgrade

--- smart-wallet-application/src/main/java/app/web/SubscriptionController.java
+++ smart-wallet-application/src/main/java/app/web/SubscriptionController.java
@@ -3,6 +3,7 @@ package app.web;
 import app.user.model.User;
 import app.user.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +17,22 @@ public class SubscriptionController {
 
     private final UserService userService;
 
+    @Autowired
     public SubscriptionController(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping
-    public String getUpgradePage() {
-       return "upgrade";
+    public ModelAndView getUpgradePage(HttpSession session) {
+
+        UUID userId = (UUID) session.getAttribute("user_id");
+        User user = userService.getById(userId);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("upgrade");
+        modelAndView.addObject("user", user);
+
+        return modelAndView;
    }
 
     @GetMapping("/history")
