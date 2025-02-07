eee80b27d6a86b295b31b2e95d2ffb223b5a860f
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-07 09:58:59 +0200

    Update SessionCheckInterceptor.java

--- smart-wallet-application/src/main/java/app/security/SessionCheckInterceptor.java
+++ smart-wallet-application/src/main/java/app/security/SessionCheckInterceptor.java
@@ -1,30 +1,34 @@
 package app.security;
 
 import app.user.model.User;
+import app.user.model.UserRole;
 import app.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 @Component
-public class SessionCheckInterceptor  implements HandlerInterceptor {
+public class SessionCheckInterceptor implements HandlerInterceptor {
 
-    private final Set<String> UNAUTHENTICATED_ENDPOINTS = Set.of("/", "/login", "/register");
+    private final Set<String> UNAUTHENTICATED_ENDPOINTS = Set.of("/", "/login", "/register", "/error");
+    private final Set<String> ADMIN_ENDPOINTS = Set.of("/users", "/reports");
 
     private final UserService userService;
 
+    @Autowired
     public SessionCheckInterceptor(UserService userService) {
         this.userService = userService;
     }
 
     // Този метод ще се изпълни преди всяка заявка
-    // HttpServletRequest request - заяката, която се праща към нашето приложение
+    // HttpServletRequest request - заявката, която се праща към нашето приложение
     // HttpServletResponse response - отговор, който връщаме
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -36,8 +40,8 @@ public class SessionCheckInterceptor  implements HandlerInterceptor {
             return true;
         }
 
-        // request.getSession(); - взема сесията, ако няма, се създава нова!
-        // request.getSession(false); - взема сесията, ако има, ако пък няма, се връща null!
+        // request.getSession() - вземам сесията, ако няма се създава нова!!!
+        // request.getSession(false) - вземам сесията, ако има, ако пък няма се връща null!!!
         HttpSession currentUserSession = request.getSession(false);
         if (currentUserSession == null) {
             response.sendRedirect("/login");
@@ -53,6 +57,25 @@ public class SessionCheckInterceptor  implements HandlerInterceptor {
             response.sendRedirect("/");
             return false;
         }
+
+        // НАЧИН 1:
+        if (ADMIN_ENDPOINTS.contains(endpoint) && user.getRole() != UserRole.ADMIN) {
+
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.getWriter().write("Access denied, you don't have the necessary permissions!");
+            return false;
+        }
+
+/*
+         НАЧИН 2:
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        if (handlerMethod.hasMethodAnnotation(RequireAdminRole.class) && user.getRole() != UserRole.ADMIN) {
+
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.getWriter().write("Access denied, you don't have the necessary permissions!");
+            return false;
+        }
+*/
 
         return true;
     }
