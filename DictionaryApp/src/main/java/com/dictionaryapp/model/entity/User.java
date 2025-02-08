f3623fb67ab10e6e42480887fa3d6cdde9081c2b
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-08 17:04:15 +0200

    adding initial DB models

---
+++ DictionaryApp/src/main/java/com/dictionaryapp/model/entity/User.java
@@ -0,0 +1,69 @@
+package com.dictionaryapp.model.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "addedBy")
+    private Set<Word> addedWords;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Word> getAddedWords() {
+        return addedWords;
+    }
+
+    public void setAddedWords(Set<Word> addedWords) {
+        this.addedWords = addedWords;
+    }
+}
---
+++ DictionaryApp/src/main/java/com/dictionaryapp/model/entity/Word.java
@@ -0,0 +1,89 @@
+package com.dictionaryapp.model.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "words")
+public class Word {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String term;
+
+    @Column(nullable = false)
+    private String translation;
+
+    private String example;
+
+    @Column(nullable = false)
+    private Instant inputDate;
+
+    @ManyToOne(optional = false)
+    private Language language;
+
+    @ManyToOne(optional = false)
+    private User addedBy;
+
+    public Word() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public void setExample(String example) {
+        this.example = example;
+    }
+
+    public Instant getInputDate() {
+        return inputDate;
+    }
+
+    public void setInputDate(Instant inputDate) {
+        this.inputDate = inputDate;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public User getAddedBy() {
+        return addedBy;
+    }
+
+    public void setAddedBy(User addedBy) {
+        this.addedBy = addedBy;
+    }
+}
