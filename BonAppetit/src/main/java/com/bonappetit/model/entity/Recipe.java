2e3b0bb3083710fa3d8be01ae7a6d7f8ed93d9d2
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-09 12:43:04 +0200

    adding db entitites

---
+++ BonAppetit/src/main/java/com/bonappetit/model/entity/Recipe.java
@@ -0,0 +1,65 @@
+package com.bonappetit.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "recipes")
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String ingredients;
+
+    @ManyToOne(optional = false)
+    private Category category;
+
+    @ManyToOne(optional = false)
+    private User addedBy;
+
+    public Recipe() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public User getAddedBy() {
+        return addedBy;
+    }
+
+    public void setAddedBy(User addedBy) {
+        this.addedBy = addedBy;
+    }
+}
---
+++ BonAppetit/src/main/java/com/bonappetit/model/entity/User.java
@@ -0,0 +1,82 @@
+package com.bonappetit.model.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "addedBy")
+    private List<Recipe> addedRecipes;
+
+    @ManyToMany
+    private List<Recipe> favouriteRecipes;
+
+    public User() {
+        this.addedRecipes = new ArrayList<>();
+        this.favouriteRecipes = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Recipe> getAddedRecipes() {
+        return addedRecipes;
+    }
+
+    public void setAddedRecipes(List<Recipe> addedRecipes) {
+        this.addedRecipes = addedRecipes;
+    }
+
+    public List<Recipe> getFavouriteRecipes() {
+        return favouriteRecipes;
+    }
+
+    public void setFavouriteRecipes(List<Recipe> favouriteRecipes) {
+        this.favouriteRecipes = favouriteRecipes;
+    }
+}
