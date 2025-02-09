6408a7ff0a9e15c55020d9583337a884829f83fe
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-09 16:21:42 +0200

    recipe update

--- BonAppetit/src/main/java/com/bonappetit/service/UserService.java
+++ BonAppetit/src/main/java/com/bonappetit/service/UserService.java
@@ -3,12 +3,15 @@ package com.bonappetit.service;
 import com.bonappetit.config.UserSession;
 import com.bonappetit.model.dto.UserLoginDTO;
 import com.bonappetit.model.dto.UserRegisterDTO;
+import com.bonappetit.model.entity.Recipe;
 import com.bonappetit.model.entity.User;
 import com.bonappetit.repo.UserRepository;
-import jakarta.validation.Valid;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +21,7 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserSession userSession;
 
+
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, UserSession userSession) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
@@ -25,8 +29,8 @@ public class UserService {
     }
 
     public boolean register(UserRegisterDTO data) {
-
-        Optional<User> existingUser = userRepository.findByUsernameOrEmail(data.getUsername(), data.getEmail());
+        Optional<User> existingUser = userRepository
+                .findByUsernameOrEmail(data.getUsername(), data.getEmail());
 
         if (existingUser.isPresent()) {
             return false;
@@ -60,5 +64,20 @@ public class UserService {
         userSession.login(byUsername.get().getId(), data.getUsername());
 
         return true;
+    }
+
+    @Transactional
+    public List<Recipe> findFavourites(Long id) {
+        return userRepository.findById(id)
+                .map(User::getFavouriteRecipes)
+                .orElseGet(ArrayList::new);
+
+//        Optional<User> byId = userRepository.findById(id);
+//
+//        if (byId.isEmpty()) {
+//            return new ArrayList<>();
+//        }
+//
+//        return byId.get().getFavouriteRecipes();
     }
 }
