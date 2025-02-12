8cfe264141ece1a37491977698bb9252733444d5
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-12 17:27:58 +0200

    Update RegisterRequest.java

--- PaintingsCollectors-App/src/main/java/com/paintingscollectors/web/dto/RegisterRequest.java
+++ PaintingsCollectors-App/src/main/java/com/paintingscollectors/web/dto/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.paintingscollectors.web.dto;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -24,4 +25,8 @@ public class RegisterRequest {
     @Size(min = 3, max = 20, message = "Password length must be between 3 and 20 characters!")
     private String confirmPassword;
 
+    @AssertTrue(message = "Password and confirm password do not match.")
+    public boolean isPasswordsMatching() {
+        return password.equals(confirmPassword);
+    }
 }
