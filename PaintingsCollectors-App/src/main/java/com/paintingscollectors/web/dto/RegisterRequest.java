37a83b84ceb8696b74fba8b43ecb61ad8cfb9a46
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-14 12:37:32 +0200

    update register request

--- PaintingsCollectors-App/src/main/java/com/paintingscollectors/web/dto/RegisterRequest.java
+++ PaintingsCollectors-App/src/main/java/com/paintingscollectors/web/dto/RegisterRequest.java
@@ -1,27 +1,24 @@
 package com.paintingscollectors.web.dto;
 
-import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
 
-    @NotNull(message = "Username must not be empty.")
+    @NotBlank(message = "Username must not be empty.")
     @Size(min = 3, max = 20, message = "Username length must be between 3 and 20 characters!")
     private String username;
 
-    @NotNull(message = "Email must not be empty.")
-    @Email(message = "Email cannot be empty!")
+    @NotBlank(message = "Email cannot be empty.")
+    @Email(message = "Not a valid email format")
     private String email;
 
-    @NotNull(message = "Password must not be empty.")
+    @NotBlank(message = "Username must not be empty.")
     @Size(min = 3, max = 20, message = "Password length must be between 3 and 20 characters!")
     private String password;
 
-    @NotNull(message = "Password must not be empty.")
+    @NotBlank(message = "Username must not be empty.")
     @Size(min = 3, max = 20, message = "Password length must be between 3 and 20 characters!")
     private String confirmPassword;
 
