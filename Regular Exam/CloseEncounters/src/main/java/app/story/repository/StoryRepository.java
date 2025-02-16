6d6e584ac9d11d64ee372b7d15f900ef52765e51
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-16 11:43:26 +0200

    some updates

--- Regular Exam/CloseEncounters/src/main/java/app/story/repository/StoryRepository.java
+++ Regular Exam/CloseEncounters/src/main/java/app/story/repository/StoryRepository.java
@@ -4,8 +4,10 @@ import app.story.model.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface StoryRepository extends JpaRepository<Story, UUID> {
+
 }
