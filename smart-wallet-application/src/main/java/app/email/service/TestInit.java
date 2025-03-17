d1d55bad05641d4a13595640eb53b491d7a42bac
Trayana Boykova <trayana.boykova@gmail.com>
2025-03-17 13:08:49 +0200

    Create TestInit.java

---
+++ smart-wallet-application/src/main/java/app/email/service/TestInit.java
@@ -0,0 +1,25 @@
+package app.email.service;
+
+import app.email.client.NotificationClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestInit implements ApplicationRunner {
+
+    private final NotificationClient notificationClient;
+
+    @Autowired
+    public TestInit(NotificationClient notificationClient) {
+        this.notificationClient = notificationClient;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        ResponseEntity<String> response = notificationClient.getHelloMessage("Trayana");
+        System.out.println(response.getBody());
+    }
+}
